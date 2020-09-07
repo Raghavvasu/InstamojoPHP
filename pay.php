5be671d7eb78733a4edfb8cbab337b03ba848e50
codingshiksha <geekygautam1997@gmail.com>
2020-09-07 12:20:57 +0530

    second commit

--- pay.php
+++ pay.php
@@ -4,13 +4,13 @@ const API_KEY ="####yourapikey####";
 const AUTH_TOKEN = "####yourauthtoken####";
 
 
-if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['amount']))
+if(isset($_POST['purpose']) && isset($_POST['name']) && isset($_POST['email']) && isset($_POST['amount']))
 {
     $api = new Instamojo\Instamojo(API_KEY, AUTH_TOKEN,'https://test.instamojo.com/api/1.1/');
     
     try {
         $response = $api->paymentRequestCreate(array(
-            "purpose" => "React.js Video Course",
+            "purpose" => $_POST['purpose'],
             "buyer_name" => $_POST['name'],
             "amount" => $_POST['amount'],
             "send_email" => true,
