f07d7ae52501360f2df960ef2d781e38cbd59753
codingshiksha <geekygautam1997@gmail.com>
2020-09-07 12:14:35 +0530

    first commit

---
+++ pay.php
@@ -0,0 +1,26 @@
+<?php
+require('./instamojo.php');
+const API_KEY ="####yourapikey####";
+const AUTH_TOKEN = "####yourauthtoken####";
+
+
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['amount']))
+{
+    $api = new Instamojo\Instamojo(API_KEY, AUTH_TOKEN,'https://test.instamojo.com/api/1.1/');
+    
+    try {
+        $response = $api->paymentRequestCreate(array(
+            "purpose" => "React.js Video Course",
+            "buyer_name" => $_POST['name'],
+            "amount" => $_POST['amount'],
+            "send_email" => true,
+            "email" => $_POST['email'],
+            "redirect_url" => "http://localhost/instamojopayment/success.html"
+            ));
+        header('Location:'. $response['longurl']);
+    }
+    catch (Exception $e) {
+        print('Error: ' . $e->getMessage());
+    }
+}
+?>
