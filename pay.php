4ea650a9f6f3f8df36ee225f6d34fc1def8a411c
Raghavvasu <45930698+Raghavvasu@users.noreply.github.com>
2021-02-07 22:17:04 +0530

    Update pay.php

--- pay.php
+++ pay.php
@@ -1,21 +1,33 @@
 <?php
-require('./instamojo.php');
-const API_KEY ="####yourapikey####";
-const AUTH_TOKEN = "####yourauthtoken####";
+require('./instamojo.php'); 
+//dev
+const API_KEY ="####yourtestapikey####";
+const AUTH_TOKEN = "####yourtestapikey####";
+
+//prod
+//const API_KEY ="####yourprodapikey####";
+//const AUTH_TOKEN = "####yourprodapikey####";
+
 
 
 if(isset($_POST['purpose']) && isset($_POST['name']) && isset($_POST['email']) && isset($_POST['amount']))
 {
+//dev
     $api = new Instamojo\Instamojo(API_KEY, AUTH_TOKEN,'https://test.instamojo.com/api/1.1/');
-    
+//prod
+//  $api = new Instamojo\Instamojo(API_KEY, AUTH_TOKEN,'https://www.instamojo.com/api/1.1/');
+   
     try {
-        $response = $api->paymentRequestCreate(array(
+        $response = $api->paymentRequestCreate(array( 
             "purpose" => $_POST['purpose'],
             "buyer_name" => $_POST['name'],
             "amount" => $_POST['amount'],
             "send_email" => true,
             "email" => $_POST['email'],
-            "redirect_url" => "http://localhost/instamojopayment/success.html"
+			//dev
+            "redirect_url" => "http://localhost/InstamojoPHP-master/success.html"
+			//prod
+		     //"redirect_url" => "http://yourdomainname/InstamojoPHP-master/success.html"
             ));
         header('Location:'. $response['longurl']);
     }
